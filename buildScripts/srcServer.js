05cfaf37572777f04f292d8e1cb8c949b3e8cc3a
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-07-20 04:06:39 +0300

    added srcServer file for express server details

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+// port initialisaton
+
